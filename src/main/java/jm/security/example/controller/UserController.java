b5d60b41c298203fa6553e93d132f5b3f9042452
kkn89 <konstantinkoblyakov@gmail.com>
2021-12-23 13:35:36 +0300

    добавление классов и зависимостей

--- src/main/java/jm/security/example/controller/UserController.java
+++ src/main/java/jm/security/example/controller/UserController.java
@@ -1,25 +1,30 @@
 package jm.security.example.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@RequestMapping("")
-public class UserController {
+@RequestMapping("/")
+public class 	UserController {
 
-    @GetMapping(value = "/")
-    public String getHomePage() {
-        return "index";
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC-SECURITY application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "hello";
     }
 
-    @GetMapping(value = "/login")
-    public String getLoginPage() {
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String loginPage() {
         return "login";
     }
 
-    @GetMapping(value = "/user")
-    public String getUserPage() {
-        return "user";
-    }
 }
