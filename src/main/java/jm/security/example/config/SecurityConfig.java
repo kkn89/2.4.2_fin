ae380474aa6eb8ade6d794582c90a343c1e15610
kkn89 <konstantinkoblyakov@gmail.com>
2021-12-30 01:13:26 +0300

    finish

--- src/main/java/jm/security/example/config/SecurityConfig.java
+++ src/main/java/jm/security/example/config/SecurityConfig.java
@@ -62,7 +62,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //указываем что будет видно ролям Admin и User
                 .hasAnyRole("ADMIN, USER")
                 .antMatchers("/**")
-                // Все что дальше /** видно только роли Admin
                 .hasAnyRole("ADMIN")
                 .and()
                 .formLogin() // Spring сам подставит свою логин форму
@@ -76,44 +75,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //выключаем кроссдоменную секьюрность
                 .and().csrf().disable();
     }
-
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("ADMIN").password("ADMIN").roles("ADMIN");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.formLogin()
-//                // указываем страницу с формой логина
-//                .loginPage("/login")
-//                //указываем логику обработки при логине
-//                .successHandler(new SuccessUserHandler())
-//                // указываем action с формы логина
-//                .loginProcessingUrl("/login")
-//                // Указываем параметры логина и пароля с формы логина
-//                .usernameParameter("j_username")
-//                .passwordParameter("j_password")
-//                // даем доступ к форме логина всем
-//                .permitAll();
-//
-//        http.logout()
-//                // разрешаем делать логаут всем
-//                .permitAll()
-//                // указываем URL логаута
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                // указываем URL при удачном логауте
-//                .logoutSuccessUrl("/login?logout")
-//                //выклчаем кроссдоменную секьюрность (на этапе обучения неважна)
-//                .and().csrf().disable();
-//
-//        http
-//                // делаем страницу регистрации недоступной для авторизированных пользователей
-//                .authorizeRequests()
-//                //страницы аутентификаци доступна всем
-//                .antMatchers("/login").anonymous()
-//                // защищенные URL
-//                .antMatchers("/hello").access("hasAnyRole('ADMIN')").anyRequest().authenticated();
-//    }
-//
 }
